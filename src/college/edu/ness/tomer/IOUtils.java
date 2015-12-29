74c11fd53503ecfa44276085eb8caeaea8b9a0d4
Dev <nessit2015@gmail.com>
2015-12-29 08:51:46 +0200

    added a method to write to files

--- src/college/edu/ness/tomer/IOUtils.java
+++ src/college/edu/ness/tomer/IOUtils.java
@@ -1,7 +1,30 @@
 package college.edu.ness.tomer;
 
-/**
- * Created by Dev on 29/12/2015.
- */
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class IOUtils {
+
+    public static void writeToFile(String contentToWriteToFile, String path){
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(path,true);
+            outputStream.write(contentToWriteToFile.getBytes());
+            outputStream.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+          if (outputStream!=null)
+              try {
+                  outputStream.close();
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+        }
+    }
 }
--- src/college/edu/ness/tomer/Main.java
+++ src/college/edu/ness/tomer/Main.java
@@ -1,8 +1,13 @@
 package college.edu.ness.tomer;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	IOUtils.writeToFile("Hello, World!\n", "1.txt");
+        File f = new File("C:\\AndroidD\\demos\\d1");
+        if (f.mkdirs())
+            System.out.println("Did it!");
     }
 }
