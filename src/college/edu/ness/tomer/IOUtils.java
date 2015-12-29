c767ba23e4a1b5dbbd161a94a98b0d9df6baced6
Dev <nessit2015@gmail.com>
2015-12-29 10:11:15 +0200

    Added a method to copy files

--- src/college/edu/ness/tomer/IOUtils.java
+++ src/college/edu/ness/tomer/IOUtils.java
@@ -4,6 +4,11 @@ import java.io.*;
 
 public class IOUtils {
 
+    /**
+     * Write A String @contentToWriteToFile to A File given a Path
+     * @param contentToWriteToFile The Content to write to the file
+     * @param path The Path of the file to write to.
+     */
     public static void writeToFile(String contentToWriteToFile, String path){
         FileOutputStream outputStream = null;
         try {
@@ -21,7 +26,11 @@ public class IOUtils {
         }
     }
 
-
+    /**
+     * Read from a file at a given @path And return A String.
+     * @param path the path of the file to be read
+     * @return A String representation of the data from the file
+     */
     public static String readFromFile(String path){
         StringBuilder builder = new StringBuilder();
         BufferedReader reader = null;
@@ -46,10 +55,47 @@ public class IOUtils {
         return null;
     }
 
-    private static void close(Closeable c){
+    /**
+     * Copy a file from the source to the destination
+     * @param from
+     * @param to
+     */
+    public static void copy(String from, String to){
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+        try {
+            fin =  new FileInputStream(from);
+            fout = new FileOutputStream(to);
+            byte[] buffer = new byte[8192];
+
+            int length = 0;
+
+
+            while ((length =  fin.read(buffer)) > 0){
+                fout.write(buffer);
+            }
+            fout.flush();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+        close(fin, fout);
+        }
+    }
+
+    /**
+     * Close all the closable objects in one place
+     * @param c The Closable to close
+     */
+    private static void close(Closeable... c){
         try {
             if (c!=null)
-                c.close();
+                for (Closeable closeable : c) {
+                    closeable.close();
+                }
         } catch (IOException e) {
             e.printStackTrace();
         }
